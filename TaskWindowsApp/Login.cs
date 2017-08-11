78fb2bf16fe03ebe845b5e6a6550ab5091076f51
Jay <jayadi.kurniawan@gmail.com>
2017-08-11 16:36:06 +0700

    Login API Implementation on Windows application

--- TaskWindowsApp/Login.cs
+++ TaskWindowsApp/Login.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,10 +20,17 @@ namespace TaskWindowsApp
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            this.Hide();
 
-            Dashboard dashbard = new Dashboard();
-            dashbard.Show();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:2460/api/Users/GetUser");
+            HttpResponseMessage response = client.GetAsync("api/Users/GetUser").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                this.Hide();
+
+                Dashboard dashbard = new Dashboard();
+                dashbard.Show();
+            }
         }
     }
 }
